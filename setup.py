8b2e6b891be3ef5e70f989e10f3fe4e4482acebf
osfunapps <osfunapps@email.com>
2019-07-11 14:23:00 +0300

    changed

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
     name='os-android-files-injector',
     packages=['os_android_files_injector'],
-    version='1.1',
-    license='MIT',
-    description='Will inject strings.xml, google-services.json, logo.png, assets and more files into an android app programmatically (dynamically)',
-    author='Oz Shabat',
-    author_email='osfunapps@gmail.com',
-    url='https://github.com/osfunapps/os-android-files-injector-py',
-    keywords=['python', 'osfunapps', 'injector', 'android', 'files', 'strings', 'logo', 'assets'],
-    install_requires=[],
+    version='1.03',  # Start with a small number and increase it with every change you make
+    license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    description='a bunch of tools for python xcode project development ',  # Give a short description about your library
+    author='Oz Shabat',  # Type in your name
+    author_email='osfunapps@gmail.com',  # Type in your E-Mail
+    url='https://github.com/osfunapps/os-xcode-tools-py',  # Provide either the link to your github or to your website
+    keywords=['python', 'osfunapps', 'files', 'xcode', 'apple', 'ios', 'tools', 'utils'],  # Keywords that define your package best
+    install_requires=['pbxproj', 'ostools'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
 
@@ -25,3 +25,4 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
 )
+
